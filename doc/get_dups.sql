a76a277fc1463f0503e948ad020cf02f3be44f7a
Andrew Chen <andrew@jaanuu.com>
2024-12-22 17:07:02 -0800

    save sql

---
+++ doc/get_dups.sql
@@ -0,0 +1,1 @@
+with dup as (select file.hash_id, count(file.file_path) as hash_count from file group by 1 having count(file.file_path) > 1) select file.*,file_hash.file_size,hash_count from file INNER join dup on file.hash_id = dup.hash_id inner join file_hash on file.hash_id=file_hash.id order by file_path asc
